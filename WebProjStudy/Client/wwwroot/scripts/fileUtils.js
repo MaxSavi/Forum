d8cdad38e7ffc7a47020bbae1204d11167024736
MaxSavi <guavaaa.mmt@gmail.com>
2023-11-21 13:39:40 +0300

    Добавьте файлы проекта.

---
+++ WebProjStudy/Client/wwwroot/scripts/fileUtils.js
@@ -0,0 +1,17 @@
+﻿window.downloadFile = function (base64String, fileName) {
+    var byteCharacters = atob(base64String);
+    var byteNumbers = new Array(byteCharacters.length);
+
+    for (var i = 0; i < byteCharacters.length; i++) {
+        byteNumbers[i] = byteCharacters.charCodeAt(i);
+    }
+
+    var byteArray = new Uint8Array(byteNumbers);
+    var blob = new Blob([byteArray], { type: "application/octet-stream" });
+    var link = document.createElement('a');
+    link.href = URL.createObjectURL(blob);
+    link.download = fileName;
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+};
